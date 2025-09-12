17de6aee4836626e0a305ff0add6876e5ef82cdf
rajanSoni0 <princesoni20200@gmail.com>
2025-09-12 22:49:24 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/",(req, res) => {
+    res.send("Hello from the main server");
+});
+
+app.get("/test",(req, res) => {
+    res.send("Hello from the test server");
+});
+
+app.get("/hello",(req, res) => {
+    res.send("Hello from the hello server");
+});
+
+
+app.listen(3000, () => {
+    console.log("Server is successfully listening on port 3000");
+});
